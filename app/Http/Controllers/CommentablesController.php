ce68a368937fd05e0564fe5c618ea13bc045354a
Mohammad Ravand <githubmohammadrepo@gmail.com>
2020-06-04 11:41:32 +0430

    add commentables table for polimorphic realtionship

---
+++ app/Http/Controllers/CommentablesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Commentables;
+use Illuminate\Http\Request;
+
+class CommentablesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Commentables  $commentables
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Commentables $commentables)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Commentables  $commentables
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Commentables $commentables)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Commentables  $commentables
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Commentables $commentables)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Commentables  $commentables
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Commentables $commentables)
+    {
+        //
+    }
+}
